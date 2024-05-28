2842b43d826ae383b89234d8ddb72e426022c8b2
okyang <owenyang123@gmail.com>
2024-05-27 21:04:33 -0700

    customize adafruit template code

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,27 @@
 // we must use software serial...
 // pin #2 is IN from sensor (TX pin on sensor), leave pin #3 disconnected
 // comment these two lines if using hardware serial
-// #include <SoftwareSerial.h>
-// SoftwareSerial pmSerial(2, 3);
+#include <SoftwareSerial.h>
+SoftwareSerial pmSerial(2, 3);
 
 Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
 
 void setup()
 {
+  // -------- Adafruit PMSA003I Air Quality Sensor Setup ----------
   // Wait for serial monitor to open
   Serial.begin(115200);
-  while (!Serial)
-    delay(10);
 
   Serial.println("Adafruit PMSA003I Air Quality Sensor");
 
   // Wait one second for sensor to boot up!
   delay(1000);
 
-  // If using serial, initialize it and set baudrate before starting!
-  // Uncomment one of the following
-  // Serial1.begin(9600);
-  // pmSerial.begin(9600);
+  // baud rate of the PM2.5 sensor is set
+  pmSerial.begin(9600);
 
-  // There are 3 options for connectivity!
-  if (!aqi.begin_I2C())
-  { // connect to the sensor over I2C
-    // if (! aqi.begin_UART(&Serial1)) { // connect to the sensor over hardware serial
-    // if (! aqi.begin_UART(&pmSerial)) { // connect to the sensor over software serial
+  if (!aqi.begin_UART(&pmSerial))
+  { // connect to the sensor over software serial
     Serial.println("Could not find PM 2.5 sensor!");
     while (1)
       delay(10);
@@ -52,40 +46,13 @@ void loop()
     delay(500); // try again in a bit!
     return;
   }
-  Serial.println("AQI reading success");
 
   Serial.println();
   Serial.println(F("---------------------------------------"));
   Serial.println(F("Concentration Units (standard)"));
   Serial.println(F("---------------------------------------"));
-  Serial.print(F("PM 1.0: "));
-  Serial.print(data.pm10_standard);
   Serial.print(F("\t\tPM 2.5: "));
   Serial.print(data.pm25_standard);
-  Serial.print(F("\t\tPM 10: "));
-  Serial.println(data.pm100_standard);
-  Serial.println(F("Concentration Units (environmental)"));
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("PM 1.0: "));
-  Serial.print(data.pm10_env);
-  Serial.print(F("\t\tPM 2.5: "));
-  Serial.print(data.pm25_env);
-  Serial.print(F("\t\tPM 10: "));
-  Serial.println(data.pm100_env);
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("Particles > 0.3um / 0.1L air:"));
-  Serial.println(data.particles_03um);
-  Serial.print(F("Particles > 0.5um / 0.1L air:"));
-  Serial.println(data.particles_05um);
-  Serial.print(F("Particles > 1.0um / 0.1L air:"));
-  Serial.println(data.particles_10um);
-  Serial.print(F("Particles > 2.5um / 0.1L air:"));
-  Serial.println(data.particles_25um);
-  Serial.print(F("Particles > 5.0um / 0.1L air:"));
-  Serial.println(data.particles_50um);
-  Serial.print(F("Particles > 10 um / 0.1L air:"));
-  Serial.println(data.particles_100um);
-  Serial.println(F("---------------------------------------"));
 
   delay(1000);
 }
